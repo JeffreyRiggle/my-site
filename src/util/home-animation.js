74871466b56e8a538caf722c6e839fc01d3b27df
Jeffrey Riggle <jeffreyriggle@gmail.com>
2026-02-17 21:25:49 -0500

    feat: Add an actual animation

--- src/util/home-animation.js
+++ src/util/home-animation.js
@@ -2,6 +2,7 @@ let opacity = 1;
 
 let characterGrid;
 let lastTime = 0;
+let lastUpdateTime = 0;
 
 export function startAnimation() {
     console.log('Starting animation');
@@ -36,12 +37,14 @@ function runAnimationLoop(canvas) {
             characterGrid.characterHeight = charSize.actualBoundingBoxAscent + charSize.actualBoundingBoxDescent;
             characterGrid.height = Math.floor(canvas.height / characterGrid.characterHeight);
             characterGrid.coordinates = [];
-            characterGrid.coordinates.push({ x: 0, y: 0, value: '0' });
-            characterGrid.coordinates.push({ x: 0, y: characterGrid.height, value: '0' });
-            characterGrid.coordinates.push({ x: characterGrid.width, y: 0, value: '0' });
-            characterGrid.coordinates.push({ x: characterGrid.width, y: characterGrid.height, value: '0' });
-            characterGrid.coordinates.push({ x: Math.floor(characterGrid.width / 2), y: Math.floor(characterGrid.height / 2), value: '0' });
         }
+
+        const lastUpdateDelta = timestamp - lastUpdateTime;
+        if (lastUpdateTime === 0 || lastUpdateDelta >= 100) {
+            lastUpdateTime = timestamp;
+            updatePattern();
+        }
+
         context.clearRect(0, 0, canvas.width, canvas.height);
 
 
@@ -61,6 +64,69 @@ function runAnimationLoop(canvas) {
         }
         lastTime = timestamp;
     });
+}
+
+function updatePattern() {
+    if (characterGrid.coordinates.length === 0) {
+        characterGrid.coordinates.push({ x: Math.floor(characterGrid.width / 2), y: Math.floor(characterGrid.height / 2), value: '0' });
+        return;
+    }
+
+    let fillMap = {};
+
+    for (let i = 0; i < characterGrid.coordinates.length; i++) {
+        const coordinate = characterGrid.coordinates[i];
+        let xSpace = fillMap[coordinate.x];
+        if (!xSpace) {
+            xSpace = {};
+        }
+        xSpace[coordinate.y] = { ...coordinate, index: i };
+        fillMap[coordinate.x] = xSpace;
+    }
+
+    let newCoordinates = [];
+
+    for (const coordinate of characterGrid.coordinates) {
+        let newCoordinateTop = { x: coordinate.x, y: coordinate.y + 1, value: '0' };
+        let newCoordinateBottom = { x: coordinate.x, y: coordinate.y - 1, value: '0' };
+        let newCoordinateLeft = { x: coordinate.x - 1, y: coordinate.y, value: '0' };
+        let newCoordinateRight = { x: coordinate.x + 1, y: coordinate.y, value: '0' };
+
+        if (shouldAddToMap(newCoordinateTop, fillMap)) {
+            newCoordinates.push(newCoordinateTop);
+            if (!fillMap[newCoordinateTop.x]) {
+                fillMap[newCoordinateTop.x] = {};
+            }
+            fillMap[newCoordinateTop.x][newCoordinateTop.y] = newCoordinateTop;
+        }
+        if (shouldAddToMap(newCoordinateBottom, fillMap)) {
+            if (!fillMap[newCoordinateBottom.x]) {
+                fillMap[newCoordinateBottom.x] = {};
+            }
+            newCoordinates.push(newCoordinateBottom);
+            fillMap[newCoordinateBottom.x][newCoordinateBottom.y] = newCoordinateBottom;
+        }
+        if (shouldAddToMap(newCoordinateLeft, fillMap)) {
+            if (!fillMap[newCoordinateLeft.x]) {
+                fillMap[newCoordinateLeft.x] = {};
+            }
+            newCoordinates.push(newCoordinateLeft);
+            fillMap[newCoordinateLeft.x][newCoordinateLeft.y] = newCoordinateLeft;
+        }
+        if (shouldAddToMap(newCoordinateRight, fillMap)) {
+            if (!fillMap[newCoordinateRight.x]) {
+                fillMap[newCoordinateRight.x] = {};
+            }
+            newCoordinates.push(newCoordinateRight);
+            fillMap[newCoordinateRight.x][newCoordinateRight.y] = newCoordinateRight;
+        }
+    }
+
+    characterGrid.coordinates = newCoordinates;
+}
+
+function shouldAddToMap(newCoordinate, fillMap) {
+    return !(fillMap[newCoordinate.x] && fillMap[newCoordinate.x][newCoordinate.y]) && newCoordinate.x <= characterGrid.width && newCoordinate.x >= 0 && newCoordinate.y <= characterGrid.height && newCoordinate.y >= 0;
 }
 
 export function stopAnimation() {
