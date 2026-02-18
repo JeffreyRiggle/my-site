963d2813b763b595df5f8cacbe24319aa0d6e737
Jeffrey Riggle <jeffreyriggle@gmail.com>
2026-02-17 20:16:58 -0500

    feat: Start storing characters in spare coordinate map

--- src/util/home-animation.js
+++ src/util/home-animation.js
@@ -1,5 +1,7 @@
 let opacity = 1;
-let displayText = '100111001';
+
+let characterGrid;
+let lastTime = 0;
 
 export function startAnimation() {
     console.log('Starting animation');
@@ -8,15 +10,17 @@ export function startAnimation() {
 
     // TODO: Do something much better
     setInterval(() => {
-        const updateIndex = Math.floor(Math.random() * displayText.length);
-        let isOn = displayText[updateIndex] == '1';
-        displayText = displayText.substring(0, updateIndex) + (isOn ? '0' : '1') + displayText.substring(updateIndex + 1, displayText.length);
+        if (!characterGrid) return;
+
+        const coordinateIndex = Math.floor(Math.random() * characterGrid.coordinates.length);
+        let coordinate = characterGrid.coordinates[coordinateIndex];
+        coordinate.value = coordinate.value == '1' ? '0' : '1';
     }, 300);
     runAnimationLoop(canvas);
 }
 
 function runAnimationLoop(canvas) {
-    requestAnimationFrame(() => {
+    requestAnimationFrame((timestamp) => {
         if (opacity > 0) {
             opacity -= .0075;
             document.body.style.setProperty('--home-opacity', opacity);
@@ -24,13 +28,38 @@ function runAnimationLoop(canvas) {
         }
         const context = canvas.getContext('2d');
 
+        if (!characterGrid) {
+            const charSize = context.measureText('0');
+            characterGrid = {};
+            characterGrid.width = Math.floor(canvas.width / charSize.width);
+            characterGrid.characterWidth = charSize.width;
+            characterGrid.characterHeight = charSize.actualBoundingBoxAscent + charSize.actualBoundingBoxDescent;
+            characterGrid.height = Math.floor(canvas.height / characterGrid.characterHeight);
+            characterGrid.coordinates = [];
+            characterGrid.coordinates.push({ x: 0, y: 0, value: '0' });
+            characterGrid.coordinates.push({ x: 0, y: characterGrid.height, value: '0' });
+            characterGrid.coordinates.push({ x: characterGrid.width, y: 0, value: '0' });
+            characterGrid.coordinates.push({ x: characterGrid.width, y: characterGrid.height, value: '0' });
+            characterGrid.coordinates.push({ x: Math.floor(characterGrid.width / 2), y: Math.floor(characterGrid.height / 2), value: '0' });
+        }
         context.clearRect(0, 0, canvas.width, canvas.height);
 
 
-        const fillSize = context.measureText(displayText).width;
         context.fillStyle = '#00a14b';
-        context.fillText(displayText, (canvas.width / 2) - (fillSize / 2), canvas.height / 2);
-        runAnimationLoop(canvas);
+        for (let coordinate of characterGrid.coordinates) {
+            context.fillText(coordinate.value, characterGrid.characterWidth * coordinate.x, (characterGrid.characterHeight * coordinate.y) + characterGrid.characterHeight);
+        }
+
+        let timeDelta = timestamp - lastTime;
+        if (lastTime == 0 || timeDelta >= 3.333) {
+            runAnimationLoop(canvas);
+        } else {
+            console.log('Resting');
+            setTimeout(() => {
+                runAnimationLoop(canvas);
+            }, 3.333 - timeDelta);
+        }
+        lastTime = timestamp;
     });
 }
 
